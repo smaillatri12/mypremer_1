9b4a9ad6f6459d6c6454d372021f9525491c38d8
smaillatri12 <smaillatri32@gmail.com>
2025-08-29 22:40:11 +0100

    دمج التعديلات مع GitHub

--- app.py
+++ app.py
@@ -41,6 +41,6 @@ def index():
         except Exception as e:
             result = {'error': str(e)}
     return render_template('index.html', result=result)
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8000)
+
